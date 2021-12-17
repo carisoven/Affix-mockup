3728115bec55bbc6c7dd069bc67a6182576d3129
Hemarat Matar <hemarat@shareinvestor.com>
2021-12-17 15:24:37 +0700

    test1

--- src/App.js
+++ src/App.js
@@ -12,24 +12,3 @@ function App() {
 
 export default App;
 
-//Discription Route page (เปลี่ยนหน้าหรือComponents)
-//--- ไม่มี Page หรือ Componets อื่นที่ปรากฎบน ตัวอย่าง
-{
-  /*
-import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to React Router!</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </div>
-  );
-}
-*/
-}
