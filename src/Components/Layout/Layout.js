fa48867c5086fc21cc1efbe52e1b6c7a22fedf89
Hemarat Matar <hemarat@shareinvestor.com>
2022-01-10 16:00:23 +0700

    hidden header

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -235,11 +235,11 @@ export default function Layout(props) {
           )}
         </Disclosure>
 
-        <header className="bg-white shadow">
+        {/* <header className="bg-white shadow">
           <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
           </div>
-        </header>
+        </header> */}
         <main>
           <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             {/* Replace with your content */}
