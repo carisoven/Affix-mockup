78383da28a4a7039cac03bc7e16645f58c7d5f2a
Hemarat Matar <patandnay766@gmail.com>
2022-01-10 16:43:55 +0700

    Import Login page templete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import { Link } from "react-router-dom";
+// import Login from "./Components/Auth/Login";
 import Layout from "./Components/Layout/Layout";
 
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Layout>
         {/* Router Page and */}
+        {/* <Login/> */}
         <div className="bg-red-200 rounded mx-auto p-4 h-full">
           <p className="text-black">Hello</p>
 
